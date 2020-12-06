b212ebc3fb7f1b1bec69a5b4d937aecbb6915658
erkosakkos <erko.sakkos@eesti.ee>
2020-12-06 03:11:45 +0200

    As a user I can transfer funds to another account

--- routes/sessions.js
+++ routes/sessions.js
@@ -6,10 +6,11 @@ const {verifyToken} = require('../middlewares')
 
 router.post('/', async (req, res, next) => {
 
-    // Get user by username from db
+    //Get user by username from db
     const user = await userModel.findOne({username: req.body.username})
-    // Validate username and password
-    if (!user || !await bcrypt.compare(req.body.password, user.password)) {
+    //Validate username and password
+    const passwordCheck = await bcrypt.compare(req.body.password, user.password)
+    if(!user || !passwordCheck ){
         return res.status(401).json({error: 'Invalid username/password'})
     }
     //Create session to db
@@ -17,24 +18,24 @@ router.post('/', async (req, res, next) => {
         userId: user.id
     })
     //Return token to user
-    return res.status(200).json({token: session._id})
+    return res.status(200).json({token: session.id})
 })
 
 router.delete('/', verifyToken, async (req, res, next) => {
-    try {
-        //Get session by userId from db
-        const session = await sessionModel.findOne({userId: req.userId})
+    try{
+    //Get session by userId from db
+    const session = await sessionModel.findOne({userId: req.userId})
 
-        //Check that session existed in db
-        if (!session) {
-            return res.status(404).json({error: 'Invalid session'})
-        }
-        //Delete session from db
-        await sessionModel.deleteOne({_id: session._id.toString()})
-        //Return token to user
-        return res.status(204).json()
-    } catch (e) {
-        return res.status(400).json({error:e.message})
+    //Check that session existed in db
+    if(!session){
+        return res.status(404).json({error: 'Invalid session'})
     }
+    //Delete session from db
+    await sessionModel.deleteOne({_id: session._id.toString()})
+    //Return token to user
+    return res.status(204).json()
+    } catch (e) {
+        return res.status(400).json({error: e.message})
+    }    
 })
 module.exports = router;
